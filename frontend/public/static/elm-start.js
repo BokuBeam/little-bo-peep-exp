7b23b75976c509d2631127c87d91d565531522dc
bokubeam <bokubeam@gmail.com>
2023-06-07 10:18:01 -0600

    Disable scroll memory on reload of page

--- frontend/public/static/elm-start.js
+++ frontend/public/static/elm-start.js
@@ -1,9 +1,10 @@
+history.scrollRestoration = "manual"
+
 const root = document.getElementById("root");
 let app = window.Elm.Main.init({ node: root });
 
 app.ports.onLoad.subscribe(function() {
-  console.log("Elm loaded");
-  // document.querySelector("#Header").scrollIntoView({ block: "start", inline: "start" });
+  document.querySelector("#Header").scrollIntoView({ block: "start", inline: "start" });
 });
 
 initMath(app);
