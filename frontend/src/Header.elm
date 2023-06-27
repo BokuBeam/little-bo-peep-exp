a690e4bc8aa4bd7d82e8c07abbb967198c0e67f9
bokubeam <bokubeam@gmail.com>
2023-06-26 22:53:17 -0600

    Adjust header padding

--- frontend/src/Header.elm
+++ frontend/src/Header.elm
@@ -14,12 +14,11 @@ view =
         , Attr.class "h-14"
         ]
         [ Html.div
-            [ Attr.class "flex justify-center content-center"
-            , Attr.class "w-full md:w-192 lg:w-full lg:grid lg:grid-cols-[2fr_3fr_2fr] pl-4"
+            [ Attr.class "w-full md:w-192 lg:w-full lg:grid lg:grid-cols-[2fr_3fr_2fr] pl-4"
             , Attr.id "Header"
             ]
             [ Html.div
-                [ Attr.class "font-clickerscript text-3xl"
+                [ Attr.class "font-clickerscript text-3xl pt-2"
                 , Attr.class "flex-auto self-end lg:col-start-2 lg:pl-2"
                 ]
                 [ Html.text "Little Bo Peep" ]
