6e526c12e0bdee728d561303c7bd3d06f86b82e2
bokubeam <bokubeam@gmail.com>
2023-06-27 16:59:38 -0600

    Only make header sticky on small screens

--- frontend/src/Header.elm
+++ frontend/src/Header.elm
@@ -10,7 +10,7 @@ view : Html msg
 view =
     Html.div
         [ Attr.class "flex justify-center items-center"
-        , Attr.class "fixed w-full bg-white z-50"
+        , Attr.class "fixed lg:absolute w-full bg-white z-50"
         , Attr.class "border-b-2 border-t-10"
         , Attr.class "h-14"
         ]
