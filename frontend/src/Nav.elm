1b0ebb35f088d1875bb08b9e0c34bdbbe0bd9657
bokubeam <bokubeam@gmail.com>
2023-06-30 21:59:28 -0600

    Parse title from elm-markup documents for nav

--- frontend/src/Nav.elm
+++ frontend/src/Nav.elm
@@ -3,6 +3,8 @@ module Nav exposing (..)
 import Articles exposing (Articles)
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Mark
+import Mark.Error
 
 
 type alias Nav =
@@ -29,22 +31,64 @@ fromArticles articles =
         Articles.toList articles
             |> List.map
                 (\( url, article ) ->
-                    { title = url
+                    { title = parse article
                     , url = url
                     }
                 )
     }
 
 
+parse : String -> String
+parse article =
+    case Mark.compile document article of
+        Mark.Success title ->
+            title
+
+        Mark.Almost { result, errors } ->
+            result
+
+        Mark.Failure errors ->
+            List.map Mark.Error.toString errors |> String.concat
+
+
+document : Mark.Document String
+document =
+    Mark.documentWith
+        (\title body ->
+            title
+        )
+        { metadata =
+            Mark.record "Article" (\title -> title)
+                |> Mark.field "title" Mark.string
+                |> Mark.toBlock
+        , body =
+            Mark.manyOf
+                [ Mark.block "Paragraph" identity Mark.string
+                , Mark.block "ParagraphFlat" identity Mark.string
+                ]
+        }
+
+
+metadata : Mark.Block { title : String }
+metadata =
+    Mark.record "Article"
+        (\title ->
+            { title = title
+            }
+        )
+        |> Mark.field "title" Mark.string
+        |> Mark.toBlock
+
+
 view : Nav -> Html msg
 view nav =
     Html.div
-        [ Attr.class "fixed right-0 z-50"
-        , Attr.class "bg-stone-300 h-full"
+        [ Attr.class "fixed right-0 z-50 p-4"
+        , Attr.class "bg-stone-100 h-full"
         ]
         [ Html.ul [] <| List.map viewEntry nav.entries ]
 
 
 viewEntry : NavEntry -> Html msg
 viewEntry entry =
-    Html.li [] [ Html.text entry.title ]
+    Html.li [] [ Html.a [ Attr.href entry.url ] [ Html.text entry.title ] ]
