911d602cb3ff25691426a2c6be048956fedab9ea
bokubeam <bokubeam@gmail.com>
2023-07-08 11:21:48 -0600

    Remove loading text

--- frontend/src/Header.elm
+++ frontend/src/Header.elm
@@ -1,4 +1,4 @@
-module Header exposing (view)
+module Header exposing (view, viewEmpty)
 
 import Html exposing (Html)
 import Html.Attributes as Attr
@@ -26,4 +26,26 @@ view navMsg nav =
                 [ Html.a [ Attr.href "/" ] [ Html.text "Little Bo Peep" ] ]
             ]
         , Nav.view navMsg nav
+        ]
+
+
+viewEmpty : Html msg
+viewEmpty =
+    Html.div
+        [ Attr.class "flex justify-center items-center"
+        , Attr.class "fixed lg:absolute w-full bg-white z-50"
+        , Attr.class "border-b border-t-10"
+        , Attr.class "h-14"
+        ]
+        [ Html.div
+            [ Attr.class "w-full md:w-192 lg:w-full pl-4"
+            , Attr.class Styles.largeGrid
+            , Attr.id "Header"
+            ]
+            [ Html.div
+                [ Attr.class "font-clickerscript text-3xl pt-2"
+                , Attr.class "self-end lg:col-start-2 lg:pl-2"
+                ]
+                [ Html.a [ Attr.href "/" ] [ Html.text "Little Bo Peep" ] ]
+            ]
         ]
